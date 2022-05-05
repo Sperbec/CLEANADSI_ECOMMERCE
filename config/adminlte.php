bccf6c39e6a7e9aabdac0f7497ae83663433bee3
Angélica Giraldo <angelicagiraldo2@gmail.com>
2022-05-05 08:53:31 -0500

    prueba2

--- config/adminlte.php
+++ config/adminlte.php
@@ -327,7 +327,7 @@ return [
             'can'     => 'pedidos',
         ],
         [
-            'text'    => 'Mi cuenta',
+            'text'    => 'Mii cuenta',
             'url'  => '/micuenta',
             'icon'    => 'fas fa-user-circle',
         ]
