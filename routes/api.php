8b38077e50d3a33730eff6a7f434cb990d4a158d
Sperbec <sperbec@gmail.com>
2022-05-13 16:18:46 -0500

    Obtener usuario mediante API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UsuarioResource;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return new UsuarioResource(User::findOrFail($id));
 });
