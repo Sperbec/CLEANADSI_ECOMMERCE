dd738af005cff04145ac62fb56101dfab8fc2fc7
Angélica Giraldo <angelicagiraldo2@gmail.com>
2022-05-16 10:09:15 -0500

    formulario de consultar ordenes de compra a proveedor

--- resources/views/ordencompra/show.blade.php
+++ resources/views/ordencompra/show.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Consultar orden de compra')
+@section('title', 'Consultar ordenes de compra')
 
 @section('content_header')
-    <h1>Consultar orden de compra a proveedor</h1>
+    <h1>COnsultar ordenes de compra a proveedor</h1>
 @stop
 
 @section('content')
