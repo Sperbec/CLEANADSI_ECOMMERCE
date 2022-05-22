18ed9b6772c89673ff0b1863bbfe00e3793a9e96
Angélica Giraldo <angelicagiraldo2@gmail.com>
2022-05-22 12:37:25 -0500

    login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,11 +4,55 @@
 <!--Reemplazar el titulo-->
 @section('title', 'Login')
 
+<link rel="stylesheet" href="{{url('/static/css/login.css')}}">
+
 
 
 <!--Mostrar sección de contenido exclusivo de esta plantilla, se debe iniciar y finalizar-->
 @section('contenido')
-<h1 style="text-align: center">Iniciar sesión</h1>
+
+<div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="/static/images/logo.png" alt="Image" class="img-fluid">
+        </div>
+        <div class="col-md-6 contents">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="mb-4">
+              <h3>Iniciar sesión</h3>
+              <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+            </div>
+            <form action="#" method="post">
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username">
+
+              </div>
+              <div class="form-group last mb-4">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+                
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center">
+               
+                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
+              </div>
+
+              <input type="submit" value="Iniciar sesión" class="btn btn-block btn-primary">
+
+          
+            </form>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+  </div>
 
 @stop
 
