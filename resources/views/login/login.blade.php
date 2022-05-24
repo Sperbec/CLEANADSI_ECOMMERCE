a071ad7bce70021d87fd4553fc1c4d041480a5c3
Angélica Giraldo <angelicagiraldo2@gmail.com>
2022-05-23 19:30:48 -0500

    login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -13,74 +13,56 @@
 <link rel="stylesheet" href="{{url('/static/css/login.css')}}">
 
 
-
 <!--Mostrar sección de contenido exclusivo de esta plantilla, se debe iniciar y finalizar-->
 @section('contenido')
 
+{!! Form::open(['url' => '/login']) !!}
 <div class="content">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <img src="/static/images/logo.png" alt="Image" class="img-fluid" width="50%">
-        </div>
-        <div class="col-md-6 contents">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="mb-4">
-              <h3>Iniciar sesión</h3>
-            </div>
-           
-            {!! Form::open(['url' => '/login']) !!}
-            <label for="username">Correo electrónico</label>
-            <div class="row">
-                <div class="form-group first">
-                    {!!  Form::email('email', null, ['class' => 'form-control', 'required']) !!}
-                </div>
-            </div>
-          
-            <label for="password">Contraseña</label>
-                <div class="row">
-                    <div class="form-group last  col-md-9">
-                        {!!  Form::password('password', ['id' => 'password', 'class' => 'form-control', 'required']) !!}
-                    </div>
-                    <div class="col-md-3">
-                        <button id="show_password" class="btn btn-secondary" type="button" onclick="mostrarPassword()"> 
-                            <span class="fa fa-eye-slash icon"></span></button>
-                    </div>
-                </div>
-            
-            <div class="row">
-                <input type="submit" value="Iniciar sesión" class="btn btn-block btn-primary">
-            </div>
-
-            <br>
 
 
-            <div class="d-flex mb-5 align-items-center">
-                <div class="align-items-center">
-                    <span class="ml-auto"><a href="{{url('/register')}}"class="forgot-pass">¿Aún no tienes una cuenta? Regístrate</a></span> 
-                 </div>
-
-                 <br>
-
-                 <div class="align-items-center">
-                    <span class="ml-auto"><a href="{{url('/recover')}}" class="forgot-pass">Recuperar contraseña</a></span> 
-                </div>
-            </div>
-
-        
-            {!!  Form::close() !!}
-
-
-            </div>
-          </div>
-          
-        </div>
-        
-      </div>
+    <div class="section-title">
+        <h3 class="title">Iniciar sesión</h3>
     </div>
-  </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <label for="email" style="float: left;">Correo electrónico:</label>
+            {!!  Form::email('email', null, ['class' => 'form-control', 'required']) !!}
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <label for="password" class="text">Contraseña:</label>
+            {!!  Form::password('password', ['id' => 'password', 'class' => 'form-control', 'required']) !!}
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6 col-md-2"></div>
+        <div class="col-6 col-md-8">
+            <input type="submit" value="Ingresar" style="margin-top: 20px;" class="btn btn-block btn-primary">
+        </div>
+    </div>
+
+    <div class="d-flex mb-5 align-items-center" style="margin-top: 20px;">
+        <div class="align-items-center">
+            <span class="ml-auto"><a href="{{url('/register')}}" class="forgot-pass">¿Aún no tienes una cuenta? Regístrate</a></span> 
+         </div>
+
+         <br>
+
+         <div class="align-items-center">
+            <span class="ml-auto"><a href="{{url('/recover')}}" class="forgot-pass">Recuperar contraseña</a></span> 
+        </div>
+    </div>
+
+
+    
+
+</div>
+{!!  Form::close() !!}
 @stop
 
 
