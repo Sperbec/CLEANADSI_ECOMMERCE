37d538072d1d2588d39049604b7f9111f9ed0cc4
jhon jairo <jjbuitrago10@misena.edu.co>
2022-05-24 20:41:02 -0500

    cambios footer

--- resources/views/frontend/sobre_nosotros.blade.php
+++ resources/views/frontend/sobre_nosotros.blade.php
@@ -23,7 +23,18 @@
     <div class="section-title">
         <h3 class="title">Sobre nosotros</h3>
     </div>
+    <p ><strong>Quienes Somos</strong>
+        <br>
+        <br>
+        <strong>ClineLine</strong> somos la empresa líder del sector comercial con más de 5 años en el mercado. Distribuimos productos de aseo personal e institucional, de acuerdo a las necesidades de los hogares y las empresas a nivel nacional.
+            <strong>ClineLine</strong> estamos avalados por <strong>INVIMA</strong> para la producción y distribución de productos de aseo y desinfección. Además, estamos certificados bajo la norma <strong>ICONTEC ISO 9001: 2015 (Sistema de Gestión de Calidad)</strong>; y de igual forma, los productos TDA, fabricados por nuestra empresa, cumplen la normatividad del <strong>Sistema de Gestión de Seguridad y Salud en el Trabajo (SG-SST) y Sistema Globalmente Armonizado (SGA)</strong>.
+        Contamos con Sedes, puntos de venta y ejecutivos comerciales en las ciudades de Cali en el valle del cauca
+    <p ><strong>CleanLine</strong></p>
+    </p>
 </div>
+
+
+
 @stop
 
 
