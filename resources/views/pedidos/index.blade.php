28e4855709d30d13a2da33794b8a2dbdff79ceea
Angélica Giraldo <angelicagiraldo2@gmail.com>
2022-05-29 15:50:17 -0500

    redireccion mis pedidos a plantilla

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1,24 +1,27 @@
-@extends('adminlte::page')
+@extends('frontend.plantilla')
 
 @section('title', 'Mis pedidos')
 
-@section('content_header')
-<div class="row">
-    <h1>Mis pedidos</h1>
+<style>
+    .content {
+        margin: 0 auto;
+        text-align: center;
+        border-radius: 10px;
+        width: 50%;
+    }
+</style>
+
+
+@section('contenido')
+
+<div class="content">
+    <div class="section-title">
+        <h3 class="title">Mis pedidos</h3>
+    </div>
 </div>
 
 @stop
 
-@section('content')
-
-
-
-@stop
-
-@section('css')
-
-@stop
-
-@section('js')
+@section('scripts')
 
 @stop
