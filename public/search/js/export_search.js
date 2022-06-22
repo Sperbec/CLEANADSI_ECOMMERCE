e48e12e2bbe1cfa547b434b327a4c2d1900a0b1b
Daar026m <darwinsantiago2003@hotmail.com>
2022-06-21 20:48:04 -0500

    fix ruta

--- public/search/js/export_search.js
+++ public/search/js/export_search.js
@@ -78,7 +78,7 @@ export class search {
 
             if (path == "/") {
                 this.ul_add_li.innerHTML +=`
-            <li id="${n+this.idli}" value="${item.nombre}" class="list-group-item"  style="">
+            <li id="${n+this.idli}" value="${item.nombre}" class="list-group-item"  class="nav-item dropdown">
             <div class="d-flex flex-row" style="">
                 <div class="p-2 text-left divimg" style="">
                     <img src="http://cleanadsi.com/api/get-img?path=${item.imagen}" width="80" height="80" >
@@ -98,7 +98,7 @@ export class search {
             <li id="${n+this.idli}" value="${item.nombre}" class="list-group-item"  style="">
             <div class="d-flex flex-row" style="">
                 <div class="p-2 text-left divimg" style="">
-                    <img src="http://cleanadsi.com/api/get-img?path=${item.imagen}" width="80" height="80" >
+                    <img src="http://cleanadsi.com/api/get-img?path=${item.imagen}" width="60" height="60" >
                 <div class="p-2">
                 <a href="../../frontend/detalle/${item.id_producto}"><strong>${nombre.substr(0,valor.length)}</strong>${nombre.substr(valor.length)}</a>
                     <p>Precio unidad= ${item.precio}</p>
