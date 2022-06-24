23550fe11281db07182ba98240c99fdf52307268
Daar026m <darwinsantiago2003@hotmail.com>
2022-06-24 14:24:03 -0500

    arreglo de css en ruta inicial

--- public/search/js/export_search.js
+++ public/search/js/export_search.js
@@ -75,22 +75,7 @@ export class search {
 
             var path = $(location).attr('pathname');
 
-            if (path == "/") {
-                this.ul_add_li.innerHTML += `
-            <li id="${n+this.idli}" value="${item.nombre}" class="list-group-item"  style="">
-            <div class="d-flex flex-row" style="">
-                <div class="p-2 text-left divimg" style="">
-                    <img src="http://cleanadsi.com/api/get-img?path=${item.imagen}" width="60" height="60" >
-                <div class="p-2">
-                <a href="frontend/detalle/${item.id_producto}"><strong>${nombre.substr(0,valor.length)}</strong>${nombre.substr(valor.length)}</a>
-                    <p>Precio unidad= ${item.precio}</p>
-                </div>
-                </div>
-            </div>
-            </li>
-            
-            `;
-            } else {
+           
                 if (cantidad > 0) {
                     n++;
 
@@ -127,7 +112,7 @@ export class search {
                         <p style="color:red;"><br>No se encontro</p>
                     `;
                 }
-            }
+            
 
         }
     }
